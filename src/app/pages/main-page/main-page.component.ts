1611068e7b98371bc95d4b2ef7cf844a772e1a07
tubyliec <antikruk@vivaldi.net>
2025-05-09 12:05:05 +0300

    feat: add header with navigation buttons

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '@components/header/header.component';
 
 @Component({
   selector: 'app-main-page',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
 })
-export class MainPageComponent {
-
-}
+export class MainPageComponent {}
