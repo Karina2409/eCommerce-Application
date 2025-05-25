3e4a15330479ca1738ec384c3b0cc688c860fa4a
tubyliec <antikruk@vivaldi.net>
2025-05-25 12:38:12 +0300

    refactor: add generic for login response

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -7,6 +7,7 @@ import { NgIf } from '@angular/common';
 import { AuthService } from '@services/auth-service';
 import { emailValidator } from '@validators/email';
 import { passwordValidator } from '@validators/password';
+import { LoginFormControlType } from '@models/types';
 
 @Component({
   selector: 'app-login-page',
@@ -20,13 +21,15 @@ export class LoginPageComponent {
   public router = inject(Router);
   public errorMessage = signal('');
   public isPasswordShown = signal(false);
-  public form: FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.required, emailValidator]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(8),
-      passwordValidator,
-    ]),
+  public form: FormGroup = new FormGroup<LoginFormControlType>({
+    email: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, emailValidator],
+    }),
+    password: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.minLength(8), passwordValidator],
+    }),
   });
 
   public onSubmitAction(): void {
