338d33f4d2865872735ee39dc144b0f94fc069b0
Karina2409 <karina.siardziuk@mail.ru>
2025-06-02 14:10:03 +0300

    feat: add user info on profile page

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -6,4 +6,24 @@ import { Component } from '@angular/core';
   templateUrl: './profile-page.component.html',
   styleUrl: './profile-page.component.scss',
 })
-export class ProfilePageComponent {}
+export class ProfilePageComponent {
+  public user;
+
+  constructor() {
+    this.user = {
+      email: 'kserduk@mail.ru',
+      firstName: 'Karina',
+      lastName: 'Siardziuk',
+      dateOfBirth: '2004-09-24',
+      addresses: [
+        {
+          id: '_ZcEfF0Q',
+          streetName: 'prospekt N 43, apt. 1',
+          postalCode: '220022',
+          city: 'Minsk',
+          country: 'BY',
+        },
+      ],
+    };
+  }
+}
