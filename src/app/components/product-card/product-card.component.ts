e17834a74bb31ce9de4d2b7e6e4a6886bbab8340
tubyliec <antikruk@vivaldi.net>
2025-06-04 01:59:03 +0300

    refactor: create product-detail-service

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -12,6 +12,7 @@ import {
   MatCardTitle,
 } from '@angular/material/card';
 import { ProductService } from '@services/product-service';
+import { ProductDetailService } from '@services/product-detail-service';
 
 @Component({
   selector: 'app-product-card',
@@ -34,6 +35,7 @@ export class ProductCardComponent implements OnInit {
   public category: string | null = '';
   public subcategory: string | null = '';
   public productService: ProductService = inject(ProductService);
+  public productDetailService: ProductDetailService = inject(ProductDetailService);
 
   constructor(
     private router: Router,
@@ -74,55 +76,6 @@ export class ProductCardComponent implements OnInit {
 
   public getName(locale = 'en-US'): string {
     return this.product.name[locale] || Object.values(this.product.name)[0];
-  }
-
-  public getDiscountedPrice(variant: ProductVariant): string | undefined {
-    void this;
-    const priceArray = variant?.prices;
-    let result;
-    if (priceArray) {
-      const centAmount = priceArray[0].discounted?.value.centAmount;
-      const fractionDigits = priceArray[0].discounted?.value?.fractionDigits;
-      const currencyCode = priceArray[0].discounted?.value?.currencyCode;
-      let amount;
-      if (centAmount && fractionDigits) {
-        amount = centAmount / Math.pow(10, fractionDigits);
-      }
-      result = `${amount?.toFixed(fractionDigits)} ${currencyCode}`;
-    }
-
-    return result;
-  }
-
-  public getAttribute(variant: ProductVariant, attribute: string, locale = 'en-US'): string | null {
-    void this;
-    const attr = variant.attributes?.find((a) => a.name === attribute);
-    if (!attr) return null;
-
-    const value = attr.value;
-
-    if (Array.isArray(value) && value.length > 0) {
-      const firstItem = value[0];
-      if (firstItem.label && typeof firstItem.label === 'object') {
-        return firstItem.label[locale] || firstItem.key || null;
-      }
-      return firstItem.key || null;
-    }
-    if (
-      value.type === 'centPrecision' &&
-      'centAmount' in value &&
-      'currencyCode' in value &&
-      'fractionDigits' in value
-    ) {
-      const amount = value.centAmount / Math.pow(10, value.fractionDigits);
-      return `${amount.toFixed(value.fractionDigits)} ${value.currencyCode}`;
-    }
-    if (value && typeof value === 'object') {
-      if ('label' in value) return value.label[locale] || value.key || null;
-      if ('key' in value) return value.key;
-    }
-
-    return null;
   }
 
   public getDescription(locale = 'en-US'): string {
--- src/app/services/index.ts
+++ src/app/services/index.ts
@@ -3,3 +3,4 @@ export * from './product-service';
 export * from './filter-service';
 export * from './sort-service';
 export * from './profile-service';
+export * from './product-detail-service';
