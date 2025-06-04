27a4d48fd825431af4b834226e3c465324f5c071
Mikhail Kirpichou <mishania261282@.ru>
2025-06-04 12:50:10 +0300

    feat:added methods to update a profile

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -42,4 +42,128 @@ export class ProfilePageComponent implements OnInit {
       defaultShippingAddressId,
     }));
   }
+
+  public async addAddress(
+    streetName: string,
+    streetNumber: string,
+    postalCode: string,
+    city: string,
+    country: string,
+  ) {
+    void this;
+    await this.profileService.updateCustomerInfo(this.user.id, {
+      version: this.profileService.currentVersion,
+      actions: [
+        {
+          action: 'addAddress',
+          address: {
+            streetName,
+            streetNumber,
+            postalCode,
+            city,
+            country,
+          },
+        },
+      ],
+    });
+  }
+
+  public async changeAddress(
+    addressId: string,
+    streetName: string,
+    streetNumber: string,
+    postalCode: string,
+    city: string,
+    country: string,
+  ) {
+    void this;
+    await this.profileService.getCustomerInfo();
+    await this.profileService.updateCustomerInfo(this.user.id, {
+      version: this.profileService.currentVersion,
+      actions: [
+        {
+          action: 'changeAddress',
+          addressId,
+          address: {
+            streetName,
+            streetNumber,
+            postalCode,
+            city,
+            country,
+          },
+        },
+      ],
+    });
+  }
+
+  public async removeAddress(addressId: string) {
+    void this;
+    await this.profileService.getCustomerInfo();
+    await this.profileService.updateCustomerInfo(this.user.id, {
+      version: this.profileService.currentVersion,
+      actions: [
+        {
+          action: 'removeAddress',
+          addressId,
+        },
+      ],
+    });
+  }
+
+  public async changeEmail(email: string) {
+    void this;
+    await this.profileService.getCustomerInfo();
+    await this.profileService.updateCustomerInfo(this.user.id, {
+      version: this.profileService.currentVersion,
+      actions: [
+        {
+          action: 'changeEmail',
+          email,
+        },
+      ],
+    });
+  }
+
+  public async changeFirstName(firstName: string) {
+    void this;
+    await this.profileService.getCustomerInfo();
+    await this.profileService.updateCustomerInfo(this.user.id, {
+      version: this.profileService.currentVersion,
+      actions: [
+        {
+          action: 'setFirstName',
+          firstName,
+        },
+      ],
+    });
+  }
+
+  public async changeLastName(lastName: string) {
+    void this;
+    await this.profileService.getCustomerInfo();
+    await this.profileService.updateCustomerInfo(this.user.id, {
+      version: this.profileService.currentVersion,
+      actions: [
+        {
+          action: 'setLastName',
+          lastName,
+        },
+      ],
+    });
+  }
+
+  //format dateOfBirth: '2015-10-21'
+  public async setDateOfBirth(dateOfBirth: string) {
+    void this;
+    await this.profileService.getCustomerInfo();
+    await this.profileService.updateCustomerInfo(this.user.id, {
+      version: this.profileService.currentVersion,
+      actions: [
+        {
+          action: 'setDateOfBirth',
+          dateOfBirth,
+        },
+      ],
+    });
+  }
 }
