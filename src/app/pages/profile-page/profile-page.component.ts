de41351ec4ced4944a1fa2b06c7ba1cb58c165b4
Mikhail Kirpichou <mishania261282@.ru>
2025-06-11 13:52:01 +0300

    feat:add message error

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -19,6 +19,7 @@ import { RouterLink } from '@angular/router';
 import { MatIcon } from '@angular/material/icon';
 import { MatTooltip } from '@angular/material/tooltip';
 import { latinValidator } from '@validators/latin';
+import { AuthService } from '@services/auth-service';
 
 @Component({
   selector: 'app-profile-page',
@@ -42,8 +43,10 @@ export class ProfilePageComponent implements OnInit {
   public user!: Customer;
   public addresses!: AddressResponse[];
   public profileService = inject(ProfileService);
+  public authService: AuthService = inject(AuthService);
   public isInfoEditing = signal(false);
   public isPasswordChanging = signal(false);
+  public message = '';
 
   public form: FormGroup = new FormGroup({
     userInfo: new FormGroup({
@@ -168,13 +171,22 @@ export class ProfilePageComponent implements OnInit {
     if (data.customer?.id) {
       id = data.customer.id;
     }
-    await this.profileService.updateCustomerPassword({
-      id: id,
-      version: this.profileService.currentVersion,
-      currentPassword: this.oldPassword.value,
-      newPassword: this.newPassword.value,
-    });
-    this.togglePasswordEditing();
+    await this.profileService
+      .updateCustomerPassword({
+        id: id,
+        version: this.profileService.currentVersion,
+        currentPassword: this.oldPassword.value,
+        newPassword: this.newPassword.value,
+      })
+      .then((result) => {
+        if (result instanceof Object && result?.success) {
+          if (data.customer?.email)
+            this.authService.signIn(data.customer?.email, this.newPassword.value);
+          this.togglePasswordEditing();
+        } else if (result instanceof Object && !result?.success) {
+          this.message = result.message;
+        }
+      });
   }
 
   public togglePasswordEditing(): void {
