491f1511269409a908a4792a482c96fe3f9b8375
tubyliec <antikruk@vivaldi.net>
2025-06-14 18:42:56 +0300

    feat: add cart control elements

--- src/app/pages/cart-page/cart-page.component.ts
+++ src/app/pages/cart-page/cart-page.component.ts
@@ -3,21 +3,35 @@ import { CurrentCart } from '@services/cart-service';
 import { LineItem } from '@commercetools/platform-sdk';
 import { NgForOf } from '@angular/common';
 import { CartProductComponent } from '@components/cart-product/cart-product.component';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-cart-page',
-  imports: [NgForOf, CartProductComponent],
+  imports: [NgForOf, CartProductComponent, FormsModule, MatButton],
   templateUrl: './cart-page.component.html',
   styleUrl: './cart-page.component.scss',
 })
 export class CartPageComponent implements OnInit {
   public cartItems = signal<LineItem[]>([]);
+  public totalPrice = signal<number>(0);
+  public currentCurrency = signal<string>('USD');
 
   public getCartItems(): void {
     this.cartItems.set(CurrentCart.products);
   }
 
+  public getTotalPrice(): void {
+    const price = CurrentCart.cart?.totalPrice;
+    if (price) {
+      const amount = price.centAmount / Math.pow(10, price.fractionDigits);
+      this.totalPrice.set(amount);
+      this.currentCurrency.set(price.currencyCode);
+    }
+  }
+
   public ngOnInit(): void {
     this.getCartItems();
+    this.getTotalPrice();
   }
 }
